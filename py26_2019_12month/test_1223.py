5fc0fb5bccaa1311ca0fea1001eb6426962ac26c
lvjunxi111 <18912945952@163.com>
2019-12-27 16:46:03 +0800

    commit message

---
+++ py26_2019_12month/test_1223.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+# @time      :2019/12/23 20:24
+# @Author    :LvJunXi
+# @Mail      :18912945952@163.com
+# @File      :test_1223.py
+# @Software  :PyCharm
+
+# 作业
+# 1、下面那些不能作为变量？
+# 1、find     2、 _num    3、7val        4、add.       5、def    6、pan      7、-print   8、open_file   9、FileName   10、9prints
+# 11、INPUT   12、ls      13、user^name  14、list1     15、str_  16、_888    17、is      18、true       19、none      20、try
+#    import keyword
+#    print(keyword.kwlist)  这些关键字是不能作为变量的
+#    不能作为变量序号：3、5、7、13、17、20
+# 2、请描述一下变量的命名规范，（简单题）
+#    1、不能是python程序关键字
+#    2、仅能有数字、字母、下划线组成
+#    3、不能以数字为开头
+# 3、python如何如何注释代码
+#    1、单行注释：#  注释内容
+#    2、多行注释：""" 注释内容 """  或者 '''注释内容'''
+#
+# 4、把学的python基本语法，总结成笔记（以后每次课都要整理）
+
+# 笔记
+import keyword
+print(keyword.kwlist)
+print("hello python")
+
+list_a = [1,2,3]
+for item in list_a:
+    print(item)
+
+d = 123
+
+
+
+
+
+
