718a30d0aee65fabae315c1afc074356c287827b
lvjunxi111 <18912945952@163.com>
2020-01-03 14:24:12 +0800

    日常提交

--- py26_2019_12month/Practice.py
+++ py26_2019_12month/Practice.py
@@ -3,9 +3,7 @@
 # @time      :2019/12/29 9:08
 # @Author    :LvJunXi
 # @Mail      :18912945952@163.com
-# @File      :11.py
+# @File      :Practice.py
 # @Software  :PyCharm
 # ----------------------------------
-list = [1, 2, 3, 2, 3, 2, 2, 2]
-list.append("1")
-print(list)
+
---
+++ py26_2019_12month/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*-coding:utf-8-*-
+# @time      :2019/12/30 19:55
+# @Author    :LvJunXi
+# @Mail      :18912945952@163.com
+# @File      :__init__.py.py
+# @Software  :PyCharm
+# ----------------------------------
