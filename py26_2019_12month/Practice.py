43b3d21debb36f387a15a2bd754ebe06a634829b
lvjunxi111 <18912945952@163.com>
2020-02-06 20:27:29 +0800

    日常提交

--- py26_2019_12month/Practice.py
+++ py26_2019_12month/Practice.py
@@ -6,19 +6,10 @@
 # @File      :Practice.py
 # @Software  :PyCharm
 # ----------------------------------
-
-
-class A:
-    def __init__(self,name,age,gender,course,classes):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.course = course
-        self.classes = classes
-
-    def add(self,classes):
-        self.classes.append(classes)
-        print(classes)
-
-
+#
+# 1、同步敲完老师上课代码（不用提交）
+# 2、将前面上课login_check这个功能函数的单元测试代码，进行代码和代码数据分离处理，用例数据保存在列表中
+# 3、通过代码读取附件文件中的register这个表单的第一行和第二行数据，想办法保存为一个字典：格式如下：
+# {'case_id': 1, 'title': '正常注册', 'data': "('python1', '123456', '123456')", 'expected': '{"code": 1, "msg": "注册成功"}', 'result': None}
+#
 
--- py26_2020_02month/test_0203_unittest_testcases.py
+++ py26_2020_02month/test_0203_unittest_testcases.py
@@ -13,7 +13,6 @@ from py26_2020_02month.test_0203_unittest_register import register
 
 class RegisterTest(unittest.TestCase):
 
-
     def test_pass(self):
         """注册成功"""
         data = ("lvjunxi", "123456", "123456")
@@ -36,7 +35,7 @@ class RegisterTest(unittest.TestCase):
 
     def test_pwd_None(self):
         """用户名不能为空"""
-        data = ("lvjunxi", None, None)
+        data = ("lvjunxi1", None, None)
         # 预期结果：{"code": 0, "msg": "所有参数不能为空"}
         expect = {"code": 0, "msg": "所有参数不能为空"}
         # 实际结果
@@ -46,7 +45,7 @@ class RegisterTest(unittest.TestCase):
 
     def test_pwd_different(self):
         """两次密码不一致"""
-        data = ("lvjunxi", "123456", "12345666")
+        data = ("lvjunxi2", "123456", "12345666")
         # 预期结果：{"code": 0, "msg": "两次密码不一致"}
         expect = {"code": 0, "msg": "两次密码不一致"}
         # 实际结果
@@ -66,7 +65,7 @@ class RegisterTest(unittest.TestCase):
 
     def test_pwd_lass_6(self):
         """密码小于6位"""
-        data = ("lvjunxi", "12345", "12345")
+        data = ("lvjunxi3", "12345", "12345")
         # 预期结果：{"code": 0, "msg": "账号和密码必须在6-18位之间"}
         expect = {"code": 0, "msg": "账号和密码必须在6-18位之间"}
         # 实际结果
@@ -76,7 +75,7 @@ class RegisterTest(unittest.TestCase):
 
     def test_pwd_greater_18(self):
         """密码大于18位"""
-        data = ("lvjunxi", "1234567891234567890", "1234567891234567890")
+        data = ("lvjunxi4", "1234567891234567890", "1234567891234567890")
         # 预期结果：{"code": 0, "msg": "账号和密码必须在6-18位之间"}
         expect = {"code": 0, "msg": "账号和密码必须在6-18位之间"}
         # 实际结果
