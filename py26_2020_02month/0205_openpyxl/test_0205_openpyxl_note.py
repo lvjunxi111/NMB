73a6064ce84d79bec776dad5901151a87a2fb98a
lvjunxi111 <18912945952@163.com>
2020-02-07 10:54:06 +0800

    日常提交

---
+++ py26_2020_02month/0205_openpyxl/test_0205_openpyxl_note.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*-coding:utf-8-*-
+# @time      :2020/2/6 20:25
+# @Author    :LvJunXi
+# @Mail      :18912945952@163.com
+# @File      :test_0205_openpyxl_note.py
+# @Software  :PyCharm
+# ----------------------------------
+import openpyxl
+import unittest
+# class RegisterTestCase(unittest.TestCase):
+#     def __init__(self, methodName, case_data):
+#         self.case_data = case_data
+#         super().__init__(methodName)  # 调用父类init方法
+
+
+# openpyxl 模块读取Excel.xlsx文件
+# wb = openpyxl.load_workbook("文件名")
